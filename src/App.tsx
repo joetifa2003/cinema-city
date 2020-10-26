1532e91fae98078cdb78364f4542af5c2b3b3d1d
joetifa2003 <62658783+joetifa2003@users.noreply.github.com>
2020-10-26 13:33:08 +0200

    adblock test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Nav from "./components/Nav/Nav";
 import { Switch } from "react-router-dom";
 import { GuardProvider, GuardedRoute } from "react-router-guards";
 import { ls } from "./utility/LocalStorage";
+import AdBlockDetect from "react-ad-block-detect";
+
 const Home = React.lazy(() => import("./pages/Home"));
 const Movie = React.lazy(() => import("./pages/Movie"));
 const Series = React.lazy(() => import("./pages/Series"));
@@ -27,6 +29,9 @@ const App = () => {
   return (
     <>
       <Nav />
+      <AdBlockDetect>
+        <div className="fixed top-0 left-0 z-50 w-full h-full bg-black"></div>
+      </AdBlockDetect>
       <Suspense fallback={<div className="w-full h-full bg-bg"></div>}>
         <GuardProvider guards={[adminAuth]}>
           <Switch>
