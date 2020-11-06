086b0b03038e90077e939240058b9a26a17d5d26
joetifa2003 <62658783+joetifa2003@users.noreply.github.com>
2020-11-06 21:01:11 +0200

    new firebase database

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,26 +1,26 @@
-import * as firebase from 'firebase/app'
+import * as firebase from "firebase/app";
 
-import 'firebase/storage'
-import 'firebase/firestore'
+import "firebase/storage";
+import "firebase/firestore";
 
 // firebase init goes here
 const firebaseConfig = {
-  apiKey: "AIzaSyCa_vIsr-DlLlAMF_LZlVf4Rnx8PXxpCPc",
-  authDomain: "cinema-city-fabb2.firebaseapp.com",
-  databaseURL: "https://cinema-city-fabb2.firebaseio.com",
-  projectId: "cinema-city-fabb2",
-  storageBucket: "cinema-city-fabb2.appspot.com",
-  messagingSenderId: "647068516174",
-  appId: "1:647068516174:web:e2d1eded6bbcd23f05281f",
-  measurementId: "G-NTNM7D8G4E"
-}
-firebase.initializeApp(firebaseConfig)
+  apiKey: "AIzaSyDQTkz9_9Buz28FJfWPDA7VCrEi3XuLEis",
+  authDomain: "cinema-city-6e100.firebaseapp.com",
+  databaseURL: "https://cinema-city-6e100.firebaseio.com",
+  projectId: "cinema-city-6e100",
+  storageBucket: "cinema-city-6e100.appspot.com",
+  messagingSenderId: "714040656071",
+  appId: "1:714040656071:web:e099e38d76673edaae7ee2",
+  measurementId: "G-SNHGQ20814",
+};
+firebase.initializeApp(firebaseConfig);
 
 // firebase utils
-const db = firebase.firestore()
-const st = firebase.storage()
+const db = firebase.firestore();
+const st = firebase.storage();
 // const auth = firebase.auth()
-const fb = firebase
+const fb = firebase;
 // db.enablePersistence()
 //   .then(() => {
 //     console.log('Offline support enabled')
@@ -29,4 +29,4 @@ const fb = firebase
 //     console.log(err)
 //   })
 
-export { db, st, fb }
+export { db, st, fb };
